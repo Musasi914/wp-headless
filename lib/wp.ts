1013522bbb7d593ea320d66139c96bb2777d1875
Hidetaka Matsuda <sopmod120@gmail.com>
2025-12-15 12:53:11 +0900

    feat(lib): WordPress Endpoint用ヘルパー関数

--- lib/wp.ts
+++ lib/wp.ts
@@ -1,1 +1,4 @@
-// https://public-api.wordpress.com/wp/v2/sites/yukiti6333.wordpress.com/posts/
+const API_BASE_URL = "https://public-api.wordpress.com/wp/v2/";
+const SITE_URL = "yukiti6333.wordpress.com";
+
+export const POSTS_ENDPOINT_URL = `${API_BASE_URL}/sites/${SITE_URL}/posts`;

d3f52c67b338ef872f0da6b96b59178b4dd44195
Hidetaka Matsuda <sopmod120@gmail.com>
2025-12-25 17:33:24 +0900

    feat(posts):iページネーション追加 ページネーション Fixes #15

--- app/posts/data.ts
+++ app/posts/data.ts
@@ -2,9 +2,14 @@ import { POSTS_ENDPOINT_URL } from "@/lib/wp";
 import { Post } from "@/types/post";
 import "server-only";
 
-export const getPosts = async () => {
-  const res = await fetch(POSTS_ENDPOINT_URL);
-  return (await res.json()) as Post[];
+export const getPosts = async (page: string) => {
+  const pageNumber = parseInt(page);
+  const res = await fetch(`${POSTS_ENDPOINT_URL}&page=${pageNumber}`);
+  const totalPages = res.headers.get("X-WP-TotalPages");
+  return {
+    posts: (await res.json()) as Post[],
+    totalPages: totalPages ? parseInt(totalPages) : 0,
+  };
 };
 
 export const getPost = async (id: string) => {
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,12 +1,21 @@
 import Link from "next/link";
 import { getPosts } from "./data";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 import { format } from "date-fns";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
-export default async function page() {
-  const posts = await getPosts();
+export default async function page({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
+  const { page = "1" } = await searchParams;
+
+  const { posts, totalPages } = await getPosts(page);
+
   return (
     <>
       <h1 className="font-bold text-2xl mb-8">記事一覧</h1>
@@ -49,6 +58,27 @@ export default async function page() {
             ></CardContent>
           </Card>
         ))}
+      </div>
+
+      <div className="flex mt-8 justify-between">
+        {Number(page) > 1 ? (
+          <Button asChild size="icon" variant="outline">
+            <Link href={`/posts?page=${Number(page) - 1}`}>
+              <ChevronLeft size={20} />
+            </Link>
+          </Button>
+        ) : (
+          <span></span>
+        )}
+        {Number(page) < totalPages ? (
+          <Button asChild size="icon" variant="outline">
+            <Link href={`/posts?page=${Number(page) + 1}`}>
+              <ChevronRight size={20} />
+            </Link>
+          </Button>
+        ) : (
+          <span></span>
+        )}
       </div>
     </>
   );

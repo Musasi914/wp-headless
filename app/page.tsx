f3357014fbda310998b0cf4b0cad43813c75c4c1
Hidetaka Matsuda <sopmod120@gmail.com>
2025-12-25 14:30:47 +0900

    feat: #12

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Link href="/posts">記事一覧</Link>
-        <Link href="/term">利用規約</Link>
-      </main>
+    <div className="">
+      <Link href="/posts">記事一覧</Link>
+      <Link href="/term">利用規約</Link>
     </div>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "yukiti6333.wordpress.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
